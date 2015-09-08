90c19afa2d802f9c81719d4051e16187ffd43d93
unknown <jliu.projects@gmail.com>
2015-09-08 02:30:31 -0400

    added personal site templat

--- personalsite/scripts/jliu.js
+++ personalsite/scripts/jliu.js
@@ -1,11 +1,16 @@
-function Set(img, title, description){
-	this.text = $("<div class='project-container'><img class='project-img' src=" + img + "><div class='project-metadata'><div class='project-title'>" + title + "</div><div class='project-description'>" + description + "</div><div class='project-launch'>Launch Site</div></div></div>");
+function Set(imgs, title, description){
+	var imgshtml = '';
+	for (var i = 0; i < imgs.length; i++){
+		imgshtml += "<img class='project-img' src=" + imgs[i] + ">";
+	}
+	this.text = $("<div class='project-container'>" + imgshtml + "<div class='project-metadata'><div class='project-title'>" + title + "</div><div class='project-description'>" + description + "</div><div class='project-launch'>Launch Site</div></div></div>");
 	// this.text.html("<img class='project-img' src=" + img + "><div class='project-title'>" + title + "</div><div class='project-description'>" + description + "</div><div class='project-launch'>Launch Site</div>");
-
+	this.text.width(imgs.length*900 + 900);
 	$('body').append(this.text);
 }
 
 var sets = [];
-sets.push(new Set("assets/kid.png", "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
-sets.push(new Set("assets/kid.png", "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
-sets.push(new Set("assets/kid.png", "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(["assets/kid.png","assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(["assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(["assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+
