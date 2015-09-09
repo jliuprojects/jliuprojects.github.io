baf2853052a706aff06e46fd9941c9b5c3cb61a8
unknown <jliu.projects@gmail.com>
2015-09-08 22:17:34 -0400

    personalsite-add some threejs interactivity

--- personalsite/scripts/jliu.js
+++ personalsite/scripts/jliu.js
@@ -12,11 +12,9 @@ function Set(index, imgs, title, description, last){
 					description + 
 					"</div><div class='project-launch'>Launch Site</div></div>" +
 					imgshtml + "</div>");
-	this.num_imgs = imgs.length;
+
 	$('#work-container').append(this.text);
 	this.text.width(this.text.width() + 490 + window.innerHeight + (window.innerWidth * 0.4));
-
-
 
 	this.text.attr( "data-" + heightSoFar, "transform: translate(0px, 0px)");
 	if (index){
@@ -67,4 +65,91 @@ $('#contact').click(function(){
 	$('#contact').css('border-bottom','solid 3px #cccccc');
 });
 
+$('.project-launch').hover(function(){
+	$('.project-launch').css('color', 'white');
+	$('.project-launch').css('background-color', 'black');
+},function(){
+	$('.project-launch').css('color', 'black');
+	$('.project-launch').css('background-color', 'white');
+});
+
 $('#about').click();
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1000);
+var mouse_pixels = {x:0,y:0};
+var renderer = new THREE.WebGLRenderer({alpha:true});
+renderer.setSize(window.innerWidth - 1, window.innerHeight - 1);
+renderer.setClearColor( 0x000000, 0 );	
+camera.position.z = 10;
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+window.addEventListener( 'mousemove', onMouseMove, false );
+
+function onMouseMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	mouse_pixels.x = event.clientX;
+	mouse_pixels.y = event.clientY;
+}
+
+document.getElementById('about-container').appendChild(renderer.domElement);
+
+var tetrahedron = new THREE.Mesh(new THREE.TetrahedronGeometry(6,0), new THREE.MeshBasicMaterial({transparent: true, opacity: 1, color: 0x000000, wireframe: true}));
+var dodecahedron = new THREE.Mesh(new THREE.DodecahedronGeometry(4,0), new THREE.MeshBasicMaterial({transparent: true, opacity: 1, color: 0x000000, wireframe: true}));
+var octahedron = new THREE.Mesh(new THREE.OctahedronGeometry(4,0), new THREE.MeshBasicMaterial({transparent: true, opacity: 1, color: 0x000000, wireframe: true}));
+
+tetrahedron.position.x = 7;
+tetrahedron.position.y = 3;
+
+dodecahedron.position.x = -1;
+dodecahedron.position.y = -3;
+
+octahedron.position.x = -7;
+octahedron.position.y = 4;
+// octahedron.position.z = -5;
+
+scene.add(tetrahedron);
+scene.add(dodecahedron);
+scene.add(octahedron);
+
+
+function render() {
+	requestAnimationFrame(render);
+	tetrahedron.rotation.x += 0.003;
+	tetrahedron.rotation.y += 0.003;
+	tetrahedron.rotation.z += 0.003;
+
+	dodecahedron.rotation.x -= 0.003;
+	dodecahedron.rotation.y -= 0.003;
+	dodecahedron.rotation.z -= 0.003;
+
+	octahedron.rotation.x += 0.003;
+	octahedron.rotation.y -= 0.003;
+	octahedron.rotation.z += 0.003;
+
+	// update the picking ray with the camera and mouse position	
+	raycaster.setFromCamera( mouse, camera );	
+
+	// calculate objects intersecting the picking ray
+	var intersects = raycaster.intersectObjects( scene.children );
+
+	for ( var i = 0; i < intersects.length; i++ ) {
+		intersects[i].object.userData.mouseoverEnterPosx = mouse_pixels.x;
+		intersects[i].object.userData.mouseoverEnterPosy = mouse_pixels.y; 
+		setTimeout(function(){ 
+			for ( var i = 0; i < intersects.length; i++ ) {
+				intersects[i].object.rotation.x += (mouse_pixels.x - intersects[i].object.userData.mouseoverEnterPosx)/10;
+				intersects[i].object.rotation.y += (mouse_pixels.y - intersects[i].object.userData.mouseoverEnterPosy)/10;
+			}
+		}, 100);
+	}
+
+
+
+	renderer.render(scene, camera);
+};
+render();
+
