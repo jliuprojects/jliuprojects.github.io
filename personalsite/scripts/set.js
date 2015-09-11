5de9a9389286588d8d5cb64ce92de04d9bc9d489
unknown <jliu.projects@gmail.com>
2015-09-11 14:10:48 -0400

    personalsite-add some images

--- personalsite/scripts/set.js
+++ personalsite/scripts/set.js
@@ -1,49 +1,50 @@
 var heightSoFar = 0;
-function Set(index, imgs, title, description, last){
-	var imgshtml = '';
-	var sign = 1;
-	var classname = 'left';
+function Set(index, imgs, widths, title, description, last){
+	this.imgshtml = '';
+	this.sign = -1;
+	this.classname = 'left';
+	this.widths = widths; //first spacing margin
 	if (index%2){
-		sign = -1;
-		classname = 'right';
+		this.sign = 1;
+		this.classname = 'right';
 	}
 	for (var i = 0; i < imgs.length; i++){
-		imgshtml += "<img class='project-img' src=" + imgs[i] + ">";
+		this.imgshtml += "<img class='project-img' src=" + imgs[i] + ">";
 	}
 
-	this.text = $("<div class='project-container-" + classname + "'>" + 
+	this.text = $("<div class='project-container-" + this.classname + "'>" + 
 					"<div class='project-metadata'><div class='project-title'>" + 
 					title + 
 					"</div><div class='project-description'>" + 
 					description + 
 					"</div><div class='project-launch'>Launch Site</div></div>" +
-					imgshtml + "</div>");
+					this.imgshtml + "</div>");
 
 	$('#work-container').append(this.text);
+	var self = this;
 
-	if (window.innerHeight < 870){
-		this.text.width(this.text.width() + 490 + 870 + (window.innerWidth * 0.4));
-	}else{
-		this.text.width(this.text.width() + 490 + window.innerHeight + (window.innerWidth * 0.4));
-	}
-	
-
-	if (index%2){
-		this.text.children('.project-metadata').css('right',(window.innerWidth * 0.10));
-		this.text.children('.project-img').css('right',(window.innerWidth * 0.4 + 32));
+	for (var i = 0; i < imgs.length; i++){
+		self.imgshtml += "<img class='project-img' src=" + imgs[i] + ">";
+		// self.widths += 870 + 200;
 	}
 
-	this.text.attr( "data-" + heightSoFar, "transform: translate(0px, 0px)");
+	self.text.width(self.widths + 500);
+
+	var endVertScroll1 = heightSoFar;
+	var endSideScroll = endVertScroll1 + self.text.width() - window.innerWidth;
+	var endVertScroll2 = endSideScroll + window.innerHeight;
+
 	if (index){
-		this.text.attr( "data-" + (heightSoFar + window.innerHeight), "transform: translate(0px, -" + window.innerHeight + "px)");
-		this.text.attr( "data-" + (heightSoFar + this.text.width() - window.innerWidth), "transform: translate(" + sign * (-1*(this.text.width() - window.innerWidth -  window.innerHeight)) + "px" + ", -" + window.innerHeight + "px)");
+		self.text.attr( "data-" + (heightSoFar - window.innerHeight), "transform: translate(0px, 0px)");
+		self.text.attr( "data-" + endVertScroll1, "transform: translate(0px, -" + window.innerHeight + "px)");
+		self.text.attr( "data-" + endSideScroll, "transform: translate(" + self.sign * (self.text.width() - window.innerWidth) + "px" + ", -" + window.innerHeight + "px)");
 		if (!last){
-			this.text.attr( "data-" + (heightSoFar + this.text.width() - window.innerWidth + window.innerHeight), "transform: translate(" + sign * (-1*(this.text.width() - window.innerWidth -  window.innerHeight)) + "px" + ", -" + window.innerHeight*2 + "px)");
+			self.text.attr( "data-" + endVertScroll2, "transform: translate(" + self.sign * (self.text.width() - window.innerWidth) + "px" + ", -" + window.innerHeight*2 + "px)");
 		}
-		heightSoFar += this.text.width() - window.innerWidth;
 	}else{
-		this.text.attr( "data-" + (heightSoFar + this.text.width() - window.innerWidth -  window.innerHeight), "transform: translate(" + (-1*(this.text.width() - window.innerWidth -  window.innerHeight)) + "px" + ", 0px)");
-		this.text.attr( "data-" + (heightSoFar + this.text.width() - window.innerWidth), "transform: translate(" + (-1*(this.text.width() - window.innerWidth -  window.innerHeight)) + "px" + ", -" + window.innerHeight + "px)");
-		heightSoFar += this.text.width() - window.innerWidth - window.innerHeight;
+		self.text.attr( "data-0", "transform: translate(0px, 0px)");
+		self.text.attr( "data-" + (heightSoFar + self.text.width() - window.innerWidth), "transform: translate(" + (-1*(self.text.width() - window.innerWidth)) + "px" + ", 0px)");
+		self.text.attr( "data-" + endVertScroll2, "transform: translate(" + (-1*(self.text.width() - window.innerWidth)) + "px" + ", -" + window.innerHeight + "px)");
 	}
+	heightSoFar = endVertScroll2;
 }
--- personalsite/scripts/work.js
+++ personalsite/scripts/work.js
@@ -1,13 +1,28 @@
 var sets = [];
-sets.push(new Set(sets.length, ["assets/kid.png","assets/kid.png","assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
-sets.push(new Set(sets.length,["assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
-sets.push(new Set(sets.length,["assets/kid.png","assets/kid.png","assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
-sets.push(new Set(sets.length,["assets/kid.png","assets/kid.png"], "Kid Studio", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ", true));
+
+sets.push(new Set(sets.length, ["assets/kid.png","assets/kid2.png","assets/kid3.png","assets/kid4.png","assets/kid5.png","assets/kid6.png"], 
+								(870+200)*6 + 200,
+								"Kid Studio", 
+								"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(sets.length,["assets/fifty6.png","assets/fifty62.png","assets/fifty63.png","assets/fifty64.png"], 
+								(870+200)*4 + 200,
+								"Fifty6", 
+								"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(sets.length,["assets/elie.png","assets/elie2.png","assets/elie3.png","assets/elie4.png"], 
+								(370+200)*4 + 200,
+								"Visionelie", 
+								"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(sets.length,["assets/kid.png","assets/kid.png"], 
+								(870+200)*2 + 200,
+								"STARZ Play", 
+								"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
+sets.push(new Set(sets.length,["assets/kid.png","assets/kid.png"], 
+								(870+200)*2 + 200,
+								"Zynga", 
+								"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ", true));
 
 sets[0].text.css({'top' : '0%'});
-
 $('.containers').fadeOut(0);
-$('body,html').css('overflow','hidden');
 
 $('#work').click(function(){
 	$('body,html').css('overflow','auto');
