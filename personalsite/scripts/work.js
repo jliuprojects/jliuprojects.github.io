8dbb963a50431f25e1227210503c80a34a9ec310
unknown <jliu.projects@gmail.com>
2015-09-15 19:36:51 -0400

    personalsite-add content

--- personalsite/scripts/work.js
+++ personalsite/scripts/work.js
@@ -4,32 +4,32 @@ sets.push(new Set(sets.length,["assets/starz.png","assets/starz2.png","assets/st
 								(870+200)*3 + 200,
 								"<div class='project-launch'>Launch In Appstore</div>",
 								"STARZ Play", 
-								"<div class='tags'>Digiflare Inc.<div class='date'>05.16 - 09.16</div></div> <div class='tags'>AngularJS, Samsung Smart TV SDK, jQuery, HTML, CSS, SASS </div> A video streaming, multi-screen TV Everywhere application for American premium cable and satellite television network provider, Starz."));
+								"<div class='tags'>Digiflare Inc.<div class='date'>05.15 - 09.15</div></div> <div class='tags'>AngularJS, Samsung Smart TV SDK, jQuery, HTML, SASS </div> A video streaming, multi-screen TV Everywhere application for American premium cable and satellite television network provider, Starz."));
 sets.push(new Set(sets.length,["assets/zynga.png","assets/zynga2.png"], 
 								(870+200)*2 + 200,
 								"",
 								"Universal Portal", 
-								"<div class='tags'>Zynga Inc. <div class='date'>08.16 - 12.16</div></div> <div class='tags'>AngularJS, NodeJS, ExpressJS, MongoDB, jQuery, HTML, CSS, SASS </div> An internal Zynga MEAN.JS portal to unite and centralize all internal services within the organization."));
+								"<div class='tags'>Zynga Inc. <div class='date'>08.14 - 12.14</div></div> <div class='tags'>AngularJS, NodeJS, ExpressJS, MongoDB, jQuery, Haml, SASS </div> An internal Zynga MEAN.JS portal to unite and centralize all internal services within the organization."));
 sets.push(new Set(sets.length,["assets/elie.png","assets/elie2.png","assets/elie3.png","assets/elie4.png"], 
 								(370+200)*4 + 200,
 								"<div class='project-launch'>Launch Mobile Site</div>",
 								"Visionelie", 
-								"<div class='tags'>Fifty6<div class='date'>05.16 - Present</div></div> <div class='tags'>Javascript, jQuery, HTML, CSS </div>Mobile website built for photographer Visionelie. Used accelerometer technology to create a spatially interactive landing page feature."));
+								"<div class='tags'>Fifty6<div class='date'>05.15 - Present</div></div> <div class='tags'>Javascript, Shopify, jQuery, HTML, CSS </div>Mobile website built for photographer Visionelie. Used accelerometer technology to create a spatially interactive landing page feature."));
 sets.push(new Set(sets.length, ["assets/kid.png","assets/kid2.png","assets/kid3.png","assets/kid4.png","assets/kid5.png","assets/kid6.png"], 
 								(870+200)*6 + 200,
 								"<div class='project-launch'>Launch Site</div>",
 								"Kid Studio", 
-								"<div class='tags'>Fifty6<div class='date'>05.16 - Present</div></div> <div class='tags'>ThreeJS, Javascript, KirbyCMS, jQuery, HTML, CSS </div>Responsive website made for film & design studio, Kid. Interactive 3D carousel feature with custom Kirby backend."));
+								"<div class='tags'>Fifty6<div class='date'>05.15 - Present</div></div> <div class='tags'>ThreeJS, Javascript, KirbyCMS, PHP, jQuery, HTML, CSS </div>Responsive website made for film & design studio, Kid. Interactive 3D carousel feature with custom Kirby backend."));
 sets.push(new Set(sets.length,["assets/fifty6.png","assets/fifty62.png","assets/fifty63.png","assets/fifty64.png"], 
 								(870+200)*4 + 200,
 								"<div class='project-launch'>Launch Site</div>",
 								"Portfolio Website", 
-								"<div class='tags'>Fifty6<div class='date'>05.16 - Present</div></div> <div class='tags'>ThreeJS, Javascript, jQuery, HTML, CSS </div> Ineractive portfolio website made for creative design studio Fifty6."));
-sets.push(new Set(sets.length,["assets/zynga.png","assets/zynga2.png"], 
-								(870+200)*2 + 200,
+								"<div class='tags'>Fifty6<div class='date'>05.15 - Present</div></div> <div class='tags'>ThreeJS, Javascript, jQuery, HTML, CSS </div> Ineractive portfolio website made for creative design studio Fifty6."));
+sets.push(new Set(sets.length,["assets/zyngasdk1.png","assets/zyngasdk2.png" ,"assets/zyngasdk3.png","assets/zyngasdk4.png","assets/zyngasdk5.png"], 
+								(684+200)*5 + 200,
 								"",
 								"Mobile Economy SDK", 
-								"<div class='tags'>Zynga Inc. <div class='date'>08.16 - 12.16</div></div><div class='tags'>AngularJS, NodeJS, ExpressJS, MongoDB, jQuery, HTML, CSS, SASS </div> An internal Zynga MEAN.JS portal to unite and centralize all internal services within the organization.", true));
+								"<div class='tags'>Zynga Inc. <div class='date'>01.14 - 4.14</div></div><div class='tags'>Objective C, Java, Ruby, Ruby On Rails, Unity Engine</div> An internal Zynga Economy SDK used in all 'With Friends' games. SDK platforms include Android, iOS and Unity clients.", true));
 
 
 
