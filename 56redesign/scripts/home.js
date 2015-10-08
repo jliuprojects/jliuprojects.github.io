16aa875cc5156c1011a0deb65c371490676c01a3
unknown <jliu.projects@gmail.com>
2015-10-08 04:33:06 -0400

    test

--- 56redesign/scripts/home.js
+++ 56redesign/scripts/home.js
@@ -2,6 +2,7 @@ var index = 0;
 var sets = [];
 var scrollLock1 = 10;
 var scrollLock2 = 10;
+var lastScrollTop = 0;
 
 var p1 = 0;
 var p2 = 0;
@@ -50,124 +51,124 @@ $( document ).ready(function() {
 	    		}, 0);
 	    	}, 0);
 	    }, 0);
+
+	    $(window).scroll(function (event) {
+		    var scroll = $(window).scrollTop();
+
+		    if (scroll > p1 - 500){
+		    	if (sets[index].getOpacity() < 1){
+			    	decrIndex(1);
+			    	sets[index].incrOpacity(0.1);
+			    	incrIndex(1);
+		    	}
+		    }
+
+		    if (scroll > p2 - 500){
+		    	if (sets[index].getOpacity() < 1){
+			    	sets[index].incrOpacity(0.1);
+		    	}
+		    }
+
+		    if (scroll >= 0 && scroll < 200){
+		    	decrIndex(2);
+		    	console.log(sets[index].getColour());
+		    	$("body").css('background-color' , sets[index].getColour());
+		    	incrIndex(2);
+		    }
+
+		    if (scroll >= p1 && scroll < p1 + 200){
+		    	decrIndex(1);
+		    	console.log(sets[index].getColour());
+		    	$("body").css('background-color' , sets[index].getColour());
+		    	incrIndex(1);
+		    }
+
+		    if (scroll >= p2 && scroll < p2 + 200){
+		    	console.log(sets[index].getColour());
+		    	$("body").css('background-color' , sets[index].getColour());
+		    }
+
+
+
+		    if (scroll > lastScrollTop){
+		    	// downscroll code
+
+		    	//scroll jacking
+		    	if (scroll >= p1 && scroll < p1 + 300){
+			    	if (scrollLock1){
+			    		$(window).scrollTop(p1);
+
+			    		scrollLock1--;
+			    		return;
+			    	}
+			    }
+			    if (scroll >= p2 && scroll < p2 + 300){
+					if (scrollLock2){
+						$(window).scrollTop(p2);
+
+						scrollLock2--;
+						return;
+					}
+				}
+
+		       	//add/remove project
+			    if (scroll + $(window).height() >= p3){
+			    	decrIndex(2);
+
+			    	sets[index].remove();
+
+			    	incrIndex(3);
+			    	sets[index].attach($(".content"));
+			    	
+			    	$(window).scrollTop(scroll - p1);
+			    	lastScrollTop = $(window).scrollTop();
+			    	scroll = $(window).scrollTop();
+			    	event.stopPropagation();
+
+			    	var tempp1 = p1;
+			    	p1 = p2 - p1;
+			    	p2 = p3 - tempp1;
+			    	setTimeout(function(){ 
+			    		p3 = $(".content").height(); 
+			    		scrollLock1 = 10;
+			    		scrollLock2 = 10;
+			    	}, 0); 
+			    }
+		   } else if (scroll < lastScrollTop){
+		    	// upscroll code
+		    	console.log("UPSCROLL");
+		    	scrollLock1 = 10;
+		    	scrollLock2 = 10;
+		    	//add/remove project
+			    if (scroll + $(window).height() <= p2 && scroll > p1){
+			    	sets[index].remove();
+			    	decrIndex(3);
+			    	sets[index].prepend($(".content"));
+			    	$(window).scrollTop($(".content").height() - $(window).height());
+			    	lastScrollTop = scroll;
+			    	incrIndex(2);
+			    	setTimeout(function(){ 
+			    		$(window).scrollTop($(".content").height() - $(window).height());
+			    		lastScrollTop = $(window).scrollTop();
+			    		scroll = $(window).scrollTop();
+			    		event.stopPropagation();
+
+				    	var tempp1 = p1;
+				    	p1 = $(".content").height() - p2;
+				    	p2 = p1 + tempp1;
+			    		p3 = $(".content").height(); 
+
+			    	}, 0); 
+			    }
+
+			}
+
+
+		   lastScrollTop = scroll;
+
+		   console.log("lock point:" + p1);
+		});
 	});
 });
 
 
-var lastScrollTop = 0;
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-
-    if (scroll > p1 - 500){
-    	if (sets[index].getOpacity() < 1){
-	    	decrIndex(1);
-	    	sets[index].incrOpacity(0.1);
-	    	incrIndex(1);
-    	}
-    }
-
-    if (scroll > p2 - 500){
-    	if (sets[index].getOpacity() < 1){
-	    	sets[index].incrOpacity(0.1);
-    	}
-    }
-
-    if (scroll >= 0 && scroll < 200){
-    	decrIndex(2);
-    	console.log(sets[index].getColour());
-    	$("body").css('background-color' , sets[index].getColour());
-    	incrIndex(2);
-    }
-
-    if (scroll >= p1 && scroll < p1 + 200){
-    	decrIndex(1);
-    	console.log(sets[index].getColour());
-    	$("body").css('background-color' , sets[index].getColour());
-    	incrIndex(1);
-    }
-
-    if (scroll >= p2 && scroll < p2 + 200){
-    	console.log(sets[index].getColour());
-    	$("body").css('background-color' , sets[index].getColour());
-    }
-
-
-
-    if (scroll > lastScrollTop){
-    	// downscroll code
-
-    	//scroll jacking
-    	if (scroll >= p1 && scroll < p1 + 300){
-	    	if (scrollLock1){
-	    		$(window).scrollTop(p1);
-
-	    		scrollLock1--;
-	    		return;
-	    	}
-	    }
-	    if (scroll >= p2 && scroll < p2 + 300){
-			if (scrollLock2){
-				$(window).scrollTop(p2);
-
-				scrollLock2--;
-				return;
-			}
-		}
-
-       	//add/remove project
-	    if (scroll + $(window).height() >= p3){
-	    	decrIndex(2);
-
-	    	sets[index].remove();
-
-	    	incrIndex(3);
-	    	sets[index].attach($(".content"));
-	    	
-	    	$(window).scrollTop(scroll - p1);
-	    	lastScrollTop = $(window).scrollTop();
-	    	scroll = $(window).scrollTop();
-	    	event.stopPropagation();
-
-	    	var tempp1 = p1;
-	    	p1 = p2 - p1;
-	    	p2 = p3 - tempp1;
-	    	setTimeout(function(){ 
-	    		p3 = $(".content").height(); 
-	    		scrollLock1 = 10;
-	    		scrollLock2 = 10;
-	    	}, 0); 
-	    }
-   } else if (scroll < lastScrollTop){
-    	// upscroll code
-    	console.log("UPSCROLL");
-    	scrollLock1 = 10;
-    	scrollLock2 = 10;
-    	//add/remove project
-	    if (scroll + $(window).height() <= p2 && scroll > p1){
-	    	sets[index].remove();
-	    	decrIndex(3);
-	    	sets[index].prepend($(".content"));
-	    	$(window).scrollTop($(".content").height() - $(window).height());
-	    	lastScrollTop = scroll;
-	    	incrIndex(2);
-	    	setTimeout(function(){ 
-	    		$(window).scrollTop($(".content").height() - $(window).height());
-	    		lastScrollTop = $(window).scrollTop();
-	    		scroll = $(window).scrollTop();
-	    		event.stopPropagation();
-
-		    	var tempp1 = p1;
-		    	p1 = $(".content").height() - p2;
-		    	p2 = p1 + tempp1;
-	    		p3 = $(".content").height(); 
-
-	    	}, 0); 
-	    }
-
-	}
-
-
-   lastScrollTop = scroll;
-
-   console.log("lock point:" + p1);
-});
