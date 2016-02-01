077f2ed8b9ad337e4c828f44925c02b6c8009f19
Jason <jliu@ifwe.co>
2016-01-31 20:19:32 -0800

    genius

--- fifty6-newest/scripts/Page.js
+++ fifty6-newest/scripts/Page.js
@@ -331,7 +331,7 @@ function onWindowResize() {
 function onDocumentMouseMove( event ) {
 
 	mouseX = event.clientX - windowHalfX;
-	mouseY = event.clientY - windowHalfY;
+	mouseY = event.clientY - windowHalfY + window.pageYOffset;
 
 }
 
