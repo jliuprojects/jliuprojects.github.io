25bc68645b7cf1fedd30333f5a10a2f34bdabf25
Jason <jliu@ifwe.co>
2016-02-01 20:38:55 -0800

    revert back to old animattitles

--- fifty6-newest/scripts/Page.js
+++ fifty6-newest/scripts/Page.js
@@ -1,7 +1,7 @@
 var projects = [];
 var focusedProject = 0;
-// var focusedTitleFixed = false;
-// var direction = "prev";
+var focusedTitleFixed = false;
+var direction = "prev";
 var bgColour = null;
 var textColour = null;
 var numProjects = 0;
@@ -57,7 +57,7 @@ function runMobile() {
 
 function render() {
 	if (!mobilecheck()) {
-		animateTitlesBetter();
+		animateTitles();
 		picturesFadeUp();
 		animateInfo();
 	}
@@ -194,94 +194,94 @@ function animateInfo() {
 	}
 }
 
-function animateTitlesBetter() {
-	for (var i = 0; i < projects.length; i++) {
-		var scroll = window.pageYOffset;
-		var bottomOfPage = scroll + window.innerHeight;
+// function animateTitlesBetter() {
+// 	for (var i = 0; i < projects.length; i++) {
+// 		var scroll = window.pageYOffset;
+// 		var bottomOfPage = scroll + window.innerHeight;
 
-		var topOfProject = projects[i].getTopPosition();
-		var bottomOfProject = projects[i].getBottomPosition();
+// 		var topOfProject = projects[i].getTopPosition();
+// 		var bottomOfProject = projects[i].getBottomPosition();
 
-		if (topOfProject <= scroll && bottomOfProject >= bottomOfPage) {
-			projects[i].fixTitle();
-			focusedProject = i;
-		} else if (topOfProject > scroll) {
-			projects[i].unfixTitle("upper");
-		} else if (bottomOfProject < bottomOfPage) {
-			projects[i].unfixTitle("lower");
-		}
-	}
-}
-
-// function animateTitles() {
-// 	var topOfFocused = projects[focusedProject].getTopPosition();
-// 	var bottomOfFocused = projects[focusedProject].getBottomPosition();
-
-// 	if (window.pageYOffset < window.innerHeight) {
-// 		for (var i = 0; i < projects.length; i++) {
+// 		if (topOfProject <= scroll && bottomOfProject >= bottomOfPage) {
+// 			projects[i].fixTitle();
+// 			focusedProject = i;
+// 		} else if (topOfProject > scroll) {
 // 			projects[i].unfixTitle("upper");
-// 		} 
-// 		focusedTitleFixed = false;
-// 		direction = "prev";
-// 	}
-
-// 	if (focusedProject == projects.length - 1) {
-// 		var topOfNext = Infinity;
-// 		var bottomOfNext = -Infinity;
-// 	} else {
-// 		var topOfNext = projects[focusedProject + 1].getTopPosition();
-// 		var bottomOfNext = projects[focusedProject + 1].getBottomPosition();
-// 	}
-
-// 	if (focusedProject == 0) {
-// 		var topOfPrev = Infinity;
-// 		var bottomOfPrev = -Infinity;
-// 	} else {
-// 		var topOfPrev = projects[focusedProject - 1].getTopPosition();
-// 		var bottomOfPrev = projects[focusedProject - 1].getBottomPosition();
-// 	}
-
-// 	if (focusedTitleFixed) { // check if the focused title is fixed, extra rc prevention
-// 		if (topOfNext < window.pageYOffset + window.innerHeight) {
-// 			// the top of the next project container hit the bottom of the screen
-// 			projects[focusedProject].unfixTitle("lower");
-// 			direction = "next";
-// 			focusedTitleFixed = false;
-// 		} else if (bottomOfPrev > window.pageYOffset) {
-// 			// the bottom of the prev project container hit the top of the screen
-// 			direction = "prev";
-// 			projects[focusedProject].unfixTitle("upper");
-// 			focusedTitleFixed = false;
-// 		}
-// 	} else {
-// 		switch (direction) {
-// 			case "next":
-// 				if (bottomOfFocused > window.pageYOffset + window.innerHeight) {
-// 					// the bottom of the focused project container hit the bottom of the screen
-// 					projects[focusedProject].fixTitle();
-// 					focusedTitleFixed = true;
-// 				} else if (topOfNext < window.pageYOffset) {
-// 					// the top of the next project container hit the top of the screen
-// 					focusedProject++;
-// 					projects[focusedProject].fixTitle();
-// 					focusedTitleFixed = true;
-// 				}
-// 				break;
-// 			case "prev":
-// 				if (topOfFocused < window.pageYOffset) {
-// 					// the top of the focused project container hit the top of the screen
-// 					projects[focusedProject].fixTitle();
-// 					focusedTitleFixed = true;
-// 				} else if (bottomOfPrev > window.pageYOffset + window.innerHeight) {
-// 					// the bottom of the prev project container hit the bottom of the screen
-// 					focusedProject--;
-// 					projects[focusedProject].fixTitle();
-// 					focusedTitleFixed = true;
-// 				}
-// 				break;
+// 		} else if (bottomOfProject < bottomOfPage) {
+// 			projects[i].unfixTitle("lower");
 // 		}
 // 	}
 // }
+
+function animateTitles() {
+	var topOfFocused = projects[focusedProject].getTopPosition();
+	var bottomOfFocused = projects[focusedProject].getBottomPosition();
+
+	if (window.pageYOffset < window.innerHeight) {
+		for (var i = 0; i < projects.length; i++) {
+			projects[i].unfixTitle("upper");
+		}
+		focusedTitleFixed = false;
+		direction = "prev";
+	}
+
+	if (focusedProject == projects.length - 1) {
+		var topOfNext = Infinity;
+		var bottomOfNext = -Infinity;
+	} else {
+		var topOfNext = projects[focusedProject + 1].getTopPosition();
+		var bottomOfNext = projects[focusedProject + 1].getBottomPosition();
+	}
+
+	if (focusedProject == 0) {
+		var topOfPrev = Infinity;
+		var bottomOfPrev = -Infinity;
+	} else {
+		var topOfPrev = projects[focusedProject - 1].getTopPosition();
+		var bottomOfPrev = projects[focusedProject - 1].getBottomPosition();
+	}
+
+	if (focusedTitleFixed) { // check if the focused title is fixed, extra rc prevention
+		if (topOfNext < window.pageYOffset + window.innerHeight) {
+			// the top of the next project container hit the bottom of the screen
+			projects[focusedProject].unfixTitle("lower");
+			direction = "next";
+			focusedTitleFixed = false;
+		} else if (bottomOfPrev > window.pageYOffset) {
+			// the bottom of the prev project container hit the top of the screen
+			direction = "prev";
+			projects[focusedProject].unfixTitle("upper");
+			focusedTitleFixed = false;
+		}
+	} else {
+		switch (direction) {
+			case "next":
+				if (bottomOfFocused > window.pageYOffset + window.innerHeight) {
+					// the bottom of the focused project container hit the bottom of the screen
+					projects[focusedProject].fixTitle();
+					focusedTitleFixed = true;
+				} else if (topOfNext < window.pageYOffset) {
+					// the top of the next project container hit the top of the screen
+					focusedProject++;
+					projects[focusedProject].fixTitle();
+					focusedTitleFixed = true;
+				}
+				break;
+			case "prev":
+				if (topOfFocused < window.pageYOffset) {
+					// the top of the focused project container hit the top of the screen
+					projects[focusedProject].fixTitle();
+					focusedTitleFixed = true;
+				} else if (bottomOfPrev > window.pageYOffset + window.innerHeight) {
+					// the bottom of the prev project container hit the bottom of the screen
+					focusedProject--;
+					projects[focusedProject].fixTitle();
+					focusedTitleFixed = true;
+				}
+				break;
+		}
+	}
+}
 
 // THREEJS
 var SEPARATION = 200, AMOUNTX = 25, AMOUNTY = 25;
