a2054495798f94929cfb839478d39b8fa2aac07c
Jason <jliu@ifwe.co>
2016-02-10 17:20:14 -0800

    pointer fade in too

--- fifty6-newest/scripts/Page.js
+++ fifty6-newest/scripts/Page.js
@@ -139,6 +139,9 @@ function animateAbout() {
 	$("#header_text").css({"top" : "-=100px"});
 	$("#header_text").animate({opacity : 1, top : "+=100px"}, 700);
 
+	$("#pointer").css({"bottom" : "-=100px"});
+	$("#pointer").animate({opacity : 1, bottom : "+=100px"}, 700);
+
 	$("#logo").css({"top" : "-=100px"});
 	$("#logo").animate({opacity : 1, top : "+=100px"}, 700, function() {
 		$("#main_title").css({"top" : "25%"});
