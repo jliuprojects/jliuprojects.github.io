23c8fdbc81a599412252afaea20e119fb23f7751
Jason <jliu@ifwe.co>
2016-03-29 00:36:04 -0700

    autocommit

---
+++ gameprototype/Player.js
@@ -0,0 +1,81 @@
+Player = function(game) {
+    Phaser.Sprite.call(this, game, 100, game.world.height - 500, 'harrison');
+    game.physics.arcade.enable(this);
+    this.animations.add('run', [1, 2, 3, 4,5]);
+    this.animations.add('stand', [0]);
+    this.animations.add('slide', [6]);
+    this.animations.add('jump', [3]);
+    this.body.gravity.y = 800;
+    this.alive = true;
+    this.sliding = true;
+
+    this.scale.setTo(0.50, 0.50); // TO DO : REMOVE THIS AND CHANGE ALL THE * 2
+    this.run();
+    game.world.add(this);
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function() {
+    // game.debug.spriteInfo(player, 20, 32);
+    game.debug.body(player);
+
+    if (!this.alive) {
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 350;
+        this.animations.play('stand', speed/40);
+        return;
+    }
+
+    if (this.x + 32 < 0) {
+        this.alive = false;
+    }
+
+    if (this.x < 160) {
+        this.body.velocity.x = speed + 100;
+    } else if (this.x > 180) {
+        this.body.velocity.x = speed - 100;
+    } else {
+        this.body.velocity.x = speed;
+    }
+    
+    if (!this.body.touching.down) {
+        this.body.velocity.x = 0;
+        this.animations.play('jump', speed/40);
+    } else if (cursors.up.isDown && this.body.touching.down) {
+        this.body.velocity.y = -500;
+    } else if (cursors.down.isDown && this.body.touching.down) {
+        this.animations.play('slide', 10);
+        this.slide();
+    } else {
+        this.animations.play('run', speed/35);
+        this.run();
+    }
+};
+
+Player.prototype.slide = function() {
+    if (!this.sliding) {
+        this.body.setSize(this.width*2, this.height*2);
+        this.body.offset.y = 100/2;
+        this.body.offset.x = 0;
+        console.log("adjusted for slide");
+        this.sliding = true;
+    }
+};
+
+Player.prototype.run = function() {
+    if (this.sliding) {
+        this.body.setSize(this.width*2, this.height*2);
+        this.body.offset.y = 0;
+        this.body.offset.x = 62/2;
+        console.log("adjusted for run");
+        this.sliding = false;
+    }
+};
+
+Player.prototype.kill = function() {
+    this.alive = false;
+}
+
--- gameprototype/main.js
+++ gameprototype/main.js
@@ -4,24 +4,22 @@ var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '');
 var grounds, platforms, clouds, bullets, player, cursors, bones, scoreText, speedText, middleGround;
 var score = 0;
 var speed = 300;
-var lastTime = Date.now();
 var levels = [];
 var currentLevel = 0;
 var levelFrame = 0;
-var dead = false;
+var dying = 0;
 
 var play = function () {};
 
 play.prototype = {
     preload: function() {
-        game.load.image('sky', 'assets/sky.png');
+        game.load.atlasJSONHash('harrison', 'assets/run.png', 'assets/run.json');
         game.load.image('ground', 'assets/ground.png');
         game.load.image('longPlatform', 'assets/longPlatform.png');
         game.load.image('medPlatform', 'assets/medPlatform.png');
         game.load.image('smallPlatform', 'assets/smallPlatform.png');
         game.load.image('stepPlatform', 'assets/stepPlatform.png');
         game.load.image('bone', 'assets/bone.png');
-        game.load.atlasJSONHash('harrison', 'assets/run.png', 'assets/run.json');
         game.load.image('background', 'assets/bg.png');
         game.load.image('bullet', 'assets/bullet.png');
         game.load.image('trees', 'assets/trees.png');
@@ -36,7 +34,7 @@ play.prototype = {
         score = 0;
         currentLevel = 0;
         levelFrame = 0;
-        dead = false;
+        dying = 0;
         speed = 300;
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.background = game.add.tileSprite(0, 0, 1920, 1080, 'background');
@@ -46,74 +44,42 @@ play.prototype = {
         scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
         speedText = game.add.text(16, 50, 'Speed: ' + speed, { fontSize: '32px', fill: '#000' });
 
+        clouds = game.add.physicsGroup();
         platforms = game.add.group();
         grounds = game.add.group();
         bullets = game.add.group();
-        clouds = game.add.physicsGroup();
+        bones = game.add.group();
+        bones.enableBody = true;
 
         var ground = new MovingStationaryObject(game, 0, game.world.height - 125, 'ground', grounds);
         var scale = gameWidth / 71 + 1;
         ground.scale.setTo(scale, 1);
 
-        var abullet = new EnemyBullet(game, game.world.width, game.world.height - 240, 'bullet', bullets);
-        abullet.scale.setTo(0.25, 0.25);
-
         var platform = new MovingStationaryObject(game, game.world.width/2, game.world.height/2, "stepPlatform", platforms);
 
         // var cloud = new MovingCloudPlatform(game, 400, 500, 'cloud-platform', clouds);
 
-        player = game.add.sprite(32, game.world.height - 500, 'harrison');
-        player.scale.setTo(0.50, 0.50);
-        game.physics.arcade.enable(player);
-        player.body.bounce.y = 0;
-        player.body.gravity.y = 800;
-        player.animations.add('run', [1, 2, 3, 4, ,5]);
-        player.animations.add('stand', [0]);
-        player.animations.add('slide', [6]);
-        player.animations.add('jump', [3]);
-        player.animations.play('stand', 10, true);
-        player.body.setSize(player.width*2, player.height*2);
-        // player.anchor.setTo(0.5, 1);
-        player.setSliding = function(sliding) {
-            if (sliding) {
-                this.body.setSize(this.width*2, this.height*2);
-                this.body.offset.y = 100/2;
-                this.body.offset.x = 0;
-                this.sliding = 60;
-            } else {
-                this.body.setSize(this.width*2, this.height*2);
-                this.body.offset.y = 0;
-                this.body.offset.x = 62/2;
-            }
-        };
-        player.setSliding(false);
+        player = new Player(game);
 
         cursors = game.input.keyboard.createCursorKeys();
-
-        bones = game.add.group();
-        bones.enableBody = true;
-        //  Here we'll create 12 of them evenly spaced apart
-        for(var i = 0; i < 1; i++) {
-            //  Create a star inside of the 'stars' group
-            var bone = bones.create(170, 0, 'bone');
-            //  Let gravity do its thing
-            bone.body.gravity.y = 800;
-            //  This just gives each star a slightly random bounce value
-            // bone.body.bounce.y = 0.7 + Math.random() * 0.2;
-        }
     },
     update: function() {
-        if (dead) {
+        if (!player.alive && !dying) {
+            // just died
+            dying = 120;
+        } else if (dying === 1) {
+            game.state.start("Play");
+        } else if (dying) {
+            dying--;
             game.physics.arcade.collide(bones, grounds);
             game.physics.arcade.collide(bones, platforms);
             return;
         }
-        // game.debug.spriteInfo(player, 20, 32);
-        // game.debug.body(player);
-        // game.debug.body(bullets.children[0]);
-        this.updateSpeed();
-        this.updateScore();
-        
+
+        score += 1 / 60;
+        speed += 2 / 60;
+        scoreText.text = 'Score: ' + score;
+        speedText.text = 'Speed: ' + speed;
 
         game.background.tilePosition.x -= 1;
         middleGround.tilePosition.x -= 2;
@@ -124,55 +90,39 @@ play.prototype = {
         game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(player, clouds);
         game.physics.arcade.overlap(player, bullets, function () {
-            dead = true;
-            player.body.velocity.x = 0;
-            window.setTimeout(function() {
-                game.state.start("Play");
-            }, 2000);
+            player.kill();
         });
         game.physics.arcade.overlap(player, bones, function(player, bone) {
             score += 50;
             bone.destroy();
         });
-
-        if (player.x + 32 < 0) {
-            dead = true;
-            player.body.velocity.x = 0;
-            window.setTimeout(function() {
-                game.state.start("Play");
-            }, 2000);
-        }
-        // game.physics.arcade.overlap(player, stars, collectStar, null, this);
         
-        player.animations.play('run', speed/35);
-
-        player.body.velocity.x = 0;
-        if (!player.body.touching.down) {
-            player.body.velocity.x = 0;
-            player.animations.play('jump', speed/40);
-        } else if (player.x < 160) {
-            player.body.velocity.x = speed + 100;
-        } else if (player.x > 180) {
-            player.body.velocity.x = speed - 100;
-        } else {
-            player.body.velocity.x = speed;
+        if (bullets.length < 1) {
+            var bullet = new EnemyBullet(game, game.world.width + 10, Math.random() * (game.world.height - 240), 'bullet', bullets);
+            bullet.scale.setTo(0.25, 0.25);
         }
 
-        if (cursors.up.isDown && player.body.touching.down) {
-            player.body.velocity.y = -500;
-        }
+        // if (clouds.length < 2) {
+        //     var cloud = new MovingCloudPlatform(game, game.world.width + 10, Math.random() * (game.world.height - 70), 'cloud-platform', clouds);
+        // }
 
-        if (!player.sliding && cursors.down.isDown && player.body.touching.down) {
-            player.animations.play('slide', 10);
-            player.setSliding(true);
-        } else if (player.sliding > 1) {
-            player.animations.play('slide', 10);
-            player.sliding--;
-        } else if (player.sliding === 1) {
-            player.setSliding(false);
-            player.sliding--;
+        this.generateLevelPlatforms();
+        this.generateGround();
+    },
+    generateGround : function() {
+        var hasground = false;
+        for (var i = 0; i < grounds.length; i++) {
+            if (grounds.children[i].x > 0) {
+                hasground = true;
+            }
         }
-
+        if (!hasground) {
+            var ground = new MovingStationaryObject(game, game.world.width, game.world.height - 125, 'ground', grounds);
+            var scale = gameWidth / 71 + 1;
+            ground.scale.setTo(scale, 1);
+        }
+    },
+    generateLevelPlatforms : function() {
         if (platforms.length) {
             if (platforms.children[platforms.length - 1].x + platforms.children[platforms.length - 1].width < game.world.width) {
                 nextPlatform = levels[currentLevel][levelFrame];
@@ -207,48 +157,7 @@ play.prototype = {
                 }
             }
         }
-        
-        var hasground = false;
-        for (var i = 0; i < grounds.length; i++) {
-            if (grounds.children[i].x > 0) {
-                hasground = true;
-            }
-        }
-        if (!hasground) {
-            var ground = new MovingStationaryObject(game, game.world.width, game.world.height - 125, 'ground', grounds);
-            var scale = gameWidth / 71 + 1;
-            ground.scale.setTo(scale, 1);
-        }
-
-        if (bullets.length < 1) {
-            var bullet = new EnemyBullet(game, game.world.width + 10, Math.random() * (game.world.height - 240), 'bullet', bullets);
-            bullet.scale.setTo(0.25, 0.25);
-        }
-
-        // if (bones.length < 40) {
-        //     var bone = bones.create(game.world.width + Math.random() * game.world.width, 100, 'bone');
-        //     bone.body.gravity.y = 400;
-        //     // bone.body.bounce.y = 0.7 + Math.random() * 0.2;
-        // }
-
-        // if (clouds.length < 2) {
-        //     var cloud = new MovingCloudPlatform(game, game.world.width + 10, Math.random() * (game.world.height - 70), 'cloud-platform', clouds);
-        // }
     },
-    updateSpeed : function() {
-        if (Date.now() - 1000 > lastTime) {
-            speed += 2;
-            speedText.text = 'Speed: ' + speed;
-        }
-
-    },
-    updateScore : function() {
-        if (Date.now() - 1000 > lastTime) {
-            score += 1;
-            scoreText.text = 'Score: ' + score;
-            lastTime = Date.now();
-        }
-    }
 }
 
 $.getJSON("levels/levels.json", function(json) {
