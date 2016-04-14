9a57b4275397d545d4ec50fa722cb3c30fd53efd
Jason <jliu@ifwe.co>
2016-04-14 16:21:20 -0700

    autocommit

--- susie/script.js
+++ susie/script.js
@@ -1,7 +1,7 @@
 var W = 350;
 var H = 450;
 
-var ball = {};
+var ball = null;
 var gravity = 0.2;
 var	bounceFactor = 0.7;
 var balls = [];
@@ -12,25 +12,6 @@ var ctx = canvas.getContext("2d");
 canvas.height = H; 
 canvas.width = W;
 
-function Ball(x, y) {
-	this.x = x;
-	this.y = y;
-	
-	this.vx = 0;
-	this.vy = 1;
-
-	this.radius = 15;
-	this.color = "red";
-};
-
-Ball.prototype.draw = function(ctx) {
-	ctx.beginPath();
-	ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-	ctx.fillStyle = this.color;
-	ctx.fill();
-	ctx.closePath();
-};
-
 function clearCanvas() {
 	ctx.clearRect(0, 0, W, H);
 };
@@ -38,23 +19,32 @@ function clearCanvas() {
 function update() {
 	clearCanvas();
 
-	balls.forEach(function(ball, index, array) {
-		ball.draw(ctx);
-		
+	if (ball != null) {
+		ctx.beginPath();
+		ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2, false);
+		ctx.fillStyle = "red";
+		ctx.fill();
+		ctx.closePath();
+
 		ball.y += ball.vy;
 		ball.vy += gravity;
 
-		if(ball.y + ball.radius > H) {
+		if (ball.y + ball.radius > H) {
 			ball.y = H - ball.radius;
 			ball.vy *= -bounceFactor;
 		}
-	});
+	}
 };
 
 canvas.addEventListener("click", function(e) {
-	var x = e.x - (window.innerWidth/2 - W/2);
-	var y = e.y;
-	balls.push(new Ball(x, y));
+	ball = {
+		x : e.x - (window.innerWidth/2 - W/2),
+		y : e.y,
+		radius : 15,
+
+		vx : 0,
+		vy : 1
+	};
 });
 
 setInterval(update, 1000/60);
